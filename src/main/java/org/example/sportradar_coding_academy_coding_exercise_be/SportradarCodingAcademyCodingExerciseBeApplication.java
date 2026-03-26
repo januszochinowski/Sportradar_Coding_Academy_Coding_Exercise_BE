f401c04593c194fdf73477d999fe0afc040aaf6d
Janusz <you@example.com>
2026-03-26 13:57:11 +0100

    Initial commit

---
+++ src/main/java/org/example/sportradar_coding_academy_coding_exercise_be/SportradarCodingAcademyCodingExerciseBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.sportradar_coding_academy_coding_exercise_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportradarCodingAcademyCodingExerciseBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportradarCodingAcademyCodingExerciseBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/sportradar_coding_academy_coding_exercise_be/SportradarCodingAcademyCodingExerciseBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sportradar_coding_academy_coding_exercise_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportradarCodingAcademyCodingExerciseBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
