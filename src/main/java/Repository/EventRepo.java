522e1ad8a2d38a68e2af6b87e74b9d9b8afbabaa
Janusz <you@example.com>
2026-03-27 12:17:57 +0100

    Add Querys to EventRepo.java

--- src/main/java/Repository/EventRepo.java
+++ src/main/java/Repository/EventRepo.java
@@ -12,12 +12,15 @@ import java.util.Date;
 
 public interface EventRepo extends JpaRepository<Event,Long> {
 
+    @Query("SELECT e FROM Event e WHERE e.place = :place")
     Page<Event> findByPlace(Place place, Pageable pageable);
 
+    @Query("SELECT e FROM Event e WHERE e.eventDate > :start ")
     Page<Event> findByAfter(Date after, Pageable pageable);
 
     @Query("SELECT e FROM Event e WHERE e.eventDate > :start AND e.eventDate < :finished")
     Page<Event> findByIdBetween(Date start, Date finished, Pageable pageable);
 
+    @Query("SELECT  e FROM Event  e WHERE e.sportName = :sportName")
     Page<Event> findBySportName(String sportName, Pageable pageable);
 }
